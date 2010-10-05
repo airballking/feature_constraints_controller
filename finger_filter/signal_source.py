418df34d357db11ae69f1b7865e6a06757d822c5
Ingo Kresse <kresse@in.tum.de>
2010-10-05 13:33:05 +0200

    code cleanup for signal_source.py

--- finger_filter/signal_source.py
+++ finger_filter/signal_source.py
@@ -1,52 +1,47 @@
 #!/usr/bin/python
 
 import yarp
+from math import *
+
+def open_port(name):
+  port = yarp.BufferedPortBottle()
+  port.open(name)
+  return port
+
+def send_value(port, value):
+  bottle = port.prepare()
+  bottle.clear()
+  bottle.addDouble(value)
+  port.write()
+
 yarp.Network.init()
 
 basename = '/signal_source'
 
-port_rect = yarp.BufferedPortBottle()
-port_rect.open(basename+'/rect')
-
-port_tri = yarp.BufferedPortBottle()
-port_tri.open(basename+'/triangle')
-
-port_saw = yarp.BufferedPortBottle()
-port_saw.open(basename+'/sawtooth')
-
-i = 0
+port_rect = open_port(basename + '/rect')
+port_tri  = open_port(basename + '/triangle')
+port_saw  = open_port(basename + '/sawtooth')
+port_sine = open_port(basename + '/sine')
 
 period = 500
-amplitude = 1.0
+amplitude = 2.5
+i = 0
 
 while True:
-  if i > period:
-    i = 0
+  i = (i + 1) % period
 
   if i < period / 2:
     rect = -amplitude
   else:
     rect = amplitude
 
-  tri = 2.0*rect*(2.0*i/period - 1.0) - amplitude
+  tri  = 2.0*rect*(2.0*i/period - 1.0) - amplitude
+  saw  = (2.0*i/period - 1.0) * amplitude
+  sine = amplitude*cos(2*pi*i/period)
 
-  saw = (2.0*i/period - 1.0) * amplitude
-
-  i += 1
-
-  bottle = port_rect.prepare()
-  bottle.clear()
-  bottle.addDouble(rect)
-  port_rect.write()
-
-  bottle = port_tri.prepare()
-  bottle.clear()
-  bottle.addDouble(tri)
-  port_tri.write()
-
-  bottle = port_saw.prepare()
-  bottle.clear()
-  bottle.addDouble(saw)
-  port_saw.write()
+  send_value(port_rect, rect)
+  send_value(port_tri,  tri)
+  send_value(port_saw,  saw)
+  send_value(port_sine, sine)
 
   yarp.Time_delay(0.005)
