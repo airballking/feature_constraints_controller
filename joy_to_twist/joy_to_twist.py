77618c310a314ee75e6f071e29107bac03eb5928
Ingo Kresse <kresse@in.tum.de>
2011-05-17 11:22:35 +0200

    added modifier buttons in joy_to_twist

--- joy_to_twist/joy_to_twist.py
+++ joy_to_twist/joy_to_twist.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from math import *
 
-# rosparam mapping:="[ lin.x, lin.y, lin.z, ang.x, ang.y, ang.z ]"
-# where each entry can be:
+# rosparam mapping<n>:="[ lin.x, lin.y, lin.z, ang.x, ang.y, ang.z ]"
+# where n is a (optional) button number and each entry can be:
 # - a positive number: use that joystick axis
 # - a negative number: use that joystick axis (inverted)
 # - a string: always send 0
@@ -13,50 +13,73 @@ import roslib; roslib.load_manifest('joy_to_twist')
 import rospy
 
 from joy.msg import Joy
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist,Vector3
 
-axis = [0]*6
-sign = [0.0]*6
+# mapping:   button  |-->  ([sign]*6, [axis]*6)
 
-def parse_mapping(param):
-  global axis, sign
-  for (i,a) in enumerate(param):
-    if type(a) in (int,long,float):
-      sign[i] = copysign(1.0, a)
-      axis[i] = int(abs(a))
-    else:
-      sign[i] = 0.0
+class Mapping:
+  def __init__(self, param):
+    """ param: a ROS param (the list of the six joystick axes) """
+    self.axis = [0]*6
+    self.sign = [0.0]*6
+    for (i,a) in enumerate(param):
+      if type(a) in (int,long,float):
+        self.sign[i] = copysign(1.0, a)
+        self.axis[i] = int(abs(a))
+      else:
+        self.sign[i] = 0.0
 
-  l = ['']*6
-  for i in range(6):
-    names = ['lin.x', 'lin.y', 'lin.z', 'rot.x', 'rot.y', 'rot.z']
-    if sign[i] != 0.0:
-      l[i] = "%s = %saxis[%d]" % (names[i], ['-',' '][sign[i] > 0], axis[i])
-    else:
-      l[i] = "%s = _" % (names[i])
+  def __repr__(self):
+    ss = [['_','+', '-'][cmp(x,0)] for x in self.sign]
+    aa = [[s, s+str(a)][s != '_'] for (s,a) in zip(ss, self.axis)]
+    lin = 'lin: [%s, %s, %s]' % (aa[0], aa[1], aa[2])
+    rot = 'rot: [%s, %s, %s]' % (aa[3], aa[4], aa[5])
+    return '['+lin+'  '+rot+']'
 
-  rospy.loginfo('['+', '.join(l)+']')
+  def __call__(self, msg):
+    """ map joystick axes onto a twist """
+    if len(msg.axes) < max(self.axis):
+      return Twist()
 
+    values = [s*msg.axes[a] for (s,a) in zip(self.sign, self.axis)]
+    return Twist(Vector3(*values[0:3]), Vector3(*values[3:6]))
 
+# handle joystick messages
 def callback(msg):
-  global axis, sign
-  if len(msg.axes) < max(axis):
-    return
-  tw = Twist()
-  tw.linear.x  = sign[0] * msg.axes[axis[0]]
-  tw.linear.y  = sign[1] * msg.axes[axis[1]]
-  tw.linear.z  = sign[2] * msg.axes[axis[2]]
-  tw.angular.x = sign[3] * msg.axes[axis[3]]
-  tw.angular.y = sign[4] * msg.axes[axis[4]]
-  tw.angular.z = sign[5] * msg.axes[axis[5]]
-  pub.publish(tw)
+  global mappings
+  for b, pressed in enumerate(msg.buttons):
+    if pressed and mappings.has_key(b):
+      pub.publish(mappings[b](msg))
+      return
+  if mappings.has_key('_'):
+    pub.publish(mappings['_'](msg))
 
+
+mappings = {}
 
 rospy.init_node('joy_to_twist')
-p = rospy.get_param('~mapping', [1, 0, '_',   '_', '_', 3])
-parse_mapping(p)
 
+name = rospy.get_name()
+params = [n for n in rospy.get_param_names() if n.startswith(name+'/mapping')]
 
+for n in params:
+  import re
+  btn = re.search('(\d*)$', n).group(0)
+  p = rospy.get_param(n, None)
+  if btn != '':
+    mappings[int(btn)] = Mapping(p)
+  else:
+    mappings['_'] = Mapping(p)
+
+# default mapping
+if len(mappings.keys()) == 0:
+  mappings['_'] = Mapping([1, 0, '_',   '_', '_', 3])
+
+# send some log output
+for k in mappings.keys():
+  rospy.loginfo('%s -> %s' % (str(k), str(mappings[k])))
+
+# connect and start
 rospy.Subscriber("/joy", Joy, callback)
 pub = rospy.Publisher('/twist', Twist)
 rospy.spin()
