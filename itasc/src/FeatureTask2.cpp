727d5280a352859eb4a793c766d2bdc7e4dd2fbe
Ingo Kresse <kresse@in.tum.de>
2011-11-29 17:14:35 +0100

    integrated feature-constraints with itasc
    well, it compiles at least...

---
+++ itasc/src/FeatureTask2.cpp
@@ -0,0 +1,175 @@
+#include "FeatureTask2.hpp"
+#include <tf_conversions/tf_kdl.h>
+#include <kdl/frames_io.hpp>
+#include <ocl/Component.hpp>
+#include <rtt/ConnPolicy.hpp>
+
+#include <ros/ros.h>
+
+#include <kdl/utilities/svd_eigen_HH.hpp>
+
+
+/*
+ * Using this macro, only one component may live
+ * in one library. If you have put your component
+ * in a namespace, don't forget to add it here too:
+ */
+ORO_CREATE_COMPONENT(FeatureTask2);
+
+#define NC_DEFAULT 6
+
+using namespace RTT;
+using namespace KDL;
+using namespace Eigen;
+
+
+// some Jacobian operator
+
+inline Twist operator*(const Jacobian& jac, const JntArray& qdot)
+{
+  assert(jac.columns() == qdot.rows());
+  Twist t;
+
+  int naxes = qdot.rows();
+  for(int i=0; i < 6; ++i)
+    for(int j=0; j < naxes; ++j)
+      t(i) += jac(i,j)*qdot(j);
+
+  return t;
+}
+
+
+FeatureTask2::FeatureTask2(const std::string& name) :
+  SubTask(name,PreOperational),
+  start_count(0),
+  guard_time(200),
+  Wy(Matrix<double,NC_DEFAULT,NC_DEFAULT>::Identity()),
+  ros_prefix("left")
+{
+  properties()->addProperty("weights_property", controller.weights).doc("local weights of the constraints");
+  properties()->addProperty("gain", controller.gains).doc("feedback gains for the constraints");
+  properties()->addProperty("ros_prefix", ros_prefix).doc("prefix for the ROS topic names");
+  properties()->addProperty("guard_time", guard_time).doc("number of cycles to wait before initialization");
+
+  controller.prepare();
+
+  nc=NC_DEFAULT;
+}
+
+// helper for connecting ports to both ROS and RTT
+template <class P> void FeatureTask2::ROS_add_port(const std::string &rtt_name,
+						     const std::string &ros_name,
+						     P &port)
+{
+  ConnPolicy c = ConnPolicy::data(ConnPolicy::LOCK_FREE, true, false);
+  c.transport = 3;
+  c.name_id = ros_name;
+
+  ports()->addPort(rtt_name, port);
+  port.createStream(c);
+}
+
+
+FeatureTask2::~FeatureTask2()
+{
+}
+
+
+bool FeatureTask2::configureHook()
+{
+  controller.prepare(nc);
+
+  Wy.diagonal() = controller.weights.data;
+  Wy_port.write(Wy);
+
+  ROS_add_port("config", ros_prefix+"/config", config_port);
+  ROS_add_port("command", ros_prefix+"/command", command_port);
+  ROS_add_port("state", ros_prefix+"/state", state_port);
+
+  command.pos_lo.resize(nc);
+  command.pos_hi.resize(nc);
+  command.weight.resize(nc);
+
+  state.jacobian.resize(nc);
+  state.chi.resize(nc);
+  state.chi_desired.resize(nc);
+  state.weights.resize(nc);
+  state.joint_names.resize(nc);
+  state.singular_values.resize(nc);
+
+  return true;
+}
+
+bool FeatureTask2::startHook(){
+
+  Logger::In in(this->getName());
+  //Check if task is well connected to the robot
+  if (!Jq_qdot_port.connected() || !T_o1_o2_port.connected()) {
+    log(Error) << "Read ports are not ready." << endlog();
+    return false;
+  }
+
+  //Initialize ports
+  SetToZero(controller.ydot);  //TODO: this should happen inside controller...
+  ydot_port.write(controller.ydot);
+
+  T_o1_o2_port.read(pose);
+
+  controller.update(pose);
+
+  ////Cf_port.write(Matrix<double, nc, nc>::Identity());
+
+  return true;
+}
+
+
+void FeatureTask2::updateHook()
+{
+  Logger::In in(this->getName());
+
+  T_o1_o2_port.read(pose);
+
+  if(NewData == command_port.read(command))
+    fromMsg(command, controller.command);
+
+  if(NewData == config_port.read(config))
+    fromMsg(config, controller.constraints);
+
+  controller.update(pose);
+
+  // for the first guard_count cycles don't move!
+  // really bad hack. should be moved to "outside"
+  if(start_count < guard_time)
+  {
+    // set desired pose to current pose
+    for(unsigned int i=0; i < nc; i++)
+    {
+      double chi = controller.chi(i);
+
+      // TODO: change these constants into attributes
+      double margin = (i < 3) ? 0.05 : 0.15;
+
+      controller.command.pos_lo(i) = chi - margin;
+      controller.command.pos_hi(i) = chi + margin;
+      controller.command.weight(i) = 1.0;
+    }
+    // don't move!
+    for(unsigned int i=0; i < nc; i++)
+      controller.ydot(i) = 0.0;
+
+    start_count++;
+  }
+
+  // interface with iTaSC
+
+  // weight coupling matrix
+  Wy.diagonal() = controller.weights.data;
+  Wy_port.write(Wy);
+
+  Jf_port.write(controller.Ht);
+  ydot_port.write(controller.ydot);
+
+  // status output for visualization and others
+  toMsg(controller, state);
+  state_port.write(state);
+}
---
+++ itasc/src/FeatureTask2.hpp
@@ -0,0 +1,57 @@
+#ifndef OROCOS_PANCAKE_BAKING_COMPONENT_HPP
+#define OROCOS_PANCAKE_BAKING_COMPONENT_HPP
+
+#include <rtt/TaskContext.hpp>
+#include <iostream>
+
+#include "SubTask.hpp"
+
+#include <feature_constraints/Controller.h>
+#include <feature_constraints/Conversions.h>
+#include <feature_constraints/FeatureConstraints.h>
+
+#include <ros/ros.h>
+
+class FeatureTask2
+  : public iTaSC::SubTask
+{
+ public:
+  FeatureTask2(string const& name);
+  ~FeatureTask2();
+  bool configureHook();
+  bool startHook();
+  void updateHook();
+  void stopHook(){};
+  void cleanupHook(){};
+
+
+private:
+  int start_count;
+  int guard_time;
+
+  KDL::Frame pose;
+  Eigen::MatrixXd Wy;
+
+  // ROS communication
+  RTT::InputPort<constraint_msgs::ConstraintConfig> config_port;
+  RTT::InputPort<constraint_msgs::ConstraintCommand> command_port;
+  RTT::OutputPort<constraint_msgs::ConstraintState> state_port;
+
+  Controller controller;
+
+  constraint_msgs::ConstraintCommand command;
+  constraint_msgs::ConstraintState state;
+  constraint_msgs::ConstraintConfig config;
+
+  // communication functions
+
+  template <class P> void ROS_add_port(const std::string &rtt_name,
+				       const std::string &ros_name,
+				       P &port);
+
+  // configuration
+  std::string ros_prefix;
+};
+
+#endif
+
