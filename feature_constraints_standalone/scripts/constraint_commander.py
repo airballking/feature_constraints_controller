6afe191da635824e5aac1e5bca0e377269bc5cbc
Ingo Kresse <kresse@in.tum.de>
2012-06-26 16:34:13 +0200

    improved joystick interface for constraints.
    * merged the joystick axis mapping into main node
    * added a stop button (0, by default)

--- feature_constraints_standalone/scripts/constraint_commander.py
+++ feature_constraints_standalone/scripts/constraint_commander.py
@@ -2,18 +2,26 @@
 
 from math import *
 
-import roslib; roslib.load_manifest('feature_constraints_standalone')
+import roslib;
+roslib.load_manifest('feature_constraints_standalone')
+roslib.load_manifest('joy_to_twist')
+
 import rospy
 
 rospy.init_node('constraint_commander')
 
-from std_msgs.msg import Float64MultiArray
-from constraint_msgs.msg import ConstraintCommand
+from sensor_msgs.msg import Joy
+from constraint_msgs.msg import ConstraintCommand, ConstraintState
 
 eps = rospy.get_param('~eps', 0.02)
 scale = rospy.get_param('~scale', 0.03)
+reset_button = rospy.get_param('~reset_button', 0)
 
+chi = []
 command = ConstraintCommand()
+
+import mapping
+button_mapping = mapping.MultiMapping(default_mapping=[0, 1, 2, 3])
 
 def command_init(values):
   command.pos_lo = [x-eps for x in values]
@@ -21,32 +29,34 @@ def command_init(values):
   command.weight = [1.0]*len(values)
 
 
-def callback_change(msg):
+def callback_joy(msg):
   global command
+  global chi
 
-  if len(command.weight) != len(msg.data):
-    command_init([0.0]*len(msg.data))
+  if msg.buttons[reset_button] == 1:
+    command_init(chi)
 
-  values = msg.data
-  for i in range(len(values)):
+  values = button_mapping(msg)
+
+  for i in range(min(len(values), len(chi))):
     command.pos_lo[i] += values[i]*scale
     command.pos_hi[i] += values[i]*scale
 
   pub.publish(command)
 
 
-def callback_set(msg):
+def callback_state(msg):
+  global chi
   global command
+  chi = msg.chi
 
-  command_init(msg.data)
-
-  pub.publish(command)
-
+  if len(command.weight) == 0:
+    command_init(chi)
 
 if __name__ == "__main__":
   # connect and start
-  sub_change = rospy.Subscriber("/velocity", Float64MultiArray, callback_change)
-  sub_set    = rospy.Subscriber("/position", Float64MultiArray, callback_set)
+  sub_change = rospy.Subscriber("/joy", Joy, callback_joy)
+  sub_state    = rospy.Subscriber("/state", ConstraintState, callback_state)
   pub = rospy.Publisher('/command', ConstraintCommand)
   rospy.spin()
 
