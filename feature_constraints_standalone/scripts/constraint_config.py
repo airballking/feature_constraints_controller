6f729017305eef9d9fa30aa89cafae2a30b22675
Ingo Kresse <kresse@in.tum.de>
2012-08-24 15:07:41 +0200

    feature & constraint visualization.

--- feature_constraints_standalone/scripts/constraint_config.py
+++ feature_constraints_standalone/scripts/constraint_config.py
@@ -7,20 +7,22 @@ import rospy
 from constraint_msgs.msg import ConstraintConfig, Constraint, Feature
 from geometry_msgs.msg import Vector3
 
+tool_center     = Feature('hand', 'center', Feature.POINT,
+                   Vector3(0,0,0), Vector3(0,0,0), Vector3(0,0,0))
 tool_front     = Feature('hand', 'front_edge', Feature.LINE,
                    Vector3(0,0,0), Vector3(0,1,0), Vector3(0,0,0))
 tool_front_rev = Feature('hand', 'front_edge_rev', Feature.LINE,
                    Vector3(0,0,0), Vector3(0,-1,0), Vector3(0,0,0))
 tool_side      = Feature('hand', 'side_edge', Feature.LINE,
                    Vector3(0,0,0), Vector3(0,0,1), Vector3(0,0,0))
-up             = Feature('hand', 'side_edge', Feature.LINE,
+up             = Feature('baker', 'side_edge', Feature.LINE,
                    Vector3(0,0,0), Vector3(0,0,1), Vector3(1,0,0))
 
 c = []
 
 c.append(Constraint('angle', 'angle',   tool_front, up))
-c.append(Constraint('dist', 'distance', tool_front, up))
-c.append(Constraint('height', 'height', tool_front, up))
+c.append(Constraint('dist', 'distance', tool_center, up))
+c.append(Constraint('height', 'height', tool_center, up))
 
 c.append(Constraint('align_front', 'perpendicular', tool_front_rev, up))
 c.append(Constraint('align_side', 'perpendicular',  tool_side,      up))
---
+++ stuff/feature_viz.py
@@ -0,0 +1,294 @@
+#!/usr/bin/python
+
+import roslib
+roslib.load_manifest('constraint_msgs')
+roslib.load_manifest('motion_viz')
+import PyKDL as kdl
+import marker
+import rospy
+import tf
+import tf_conversions.posemath as pm
+
+from constraint_msgs.msg import ConstraintConfig, Constraint, Feature
+from geometry_msgs.msg import Point, Vector3
+from std_msgs.msg import ColorRGBA
+
+
+grey    = ColorRGBA(0.7, 0.7, 0.7, 0.5)
+red     = ColorRGBA(0.7, 0.1, 0.1, 1.0)
+yellow  = ColorRGBA(0.7, 0.7, 0.1, 1.0)
+
+_config_ = {'line_width': 0.05,
+            'frame_id': '/base_link',
+            'ns': 'features',
+            'marker_color': grey,
+            'marker_id': 0}
+
+
+def msg2vec(v):
+  """Convert a geometry_msgs/Vector3 to a KDL Vector."""
+  return kdl.Vector(v.x, v.y, v.z)
+
+
+def msg2feature(msg):
+  """Convert a constraint_msgs/Feature to a Feature."""
+  pos = msg2vec(msg.position)
+  dir = msg2vec(msg.direction)
+  return Feature(msg.type, pos, dir, msg.frame_id)
+
+
+def marker_base(**args):
+  global _config_
+
+  if args.has_key('line_width'):
+    line_width = args.pop('line_width')
+  else:
+    line_width = _config_['line_width']
+
+  m = marker.Marker(id=_config_['marker_id'],
+                    ns=_config_['ns'], **args)
+  m.header.frame_id = _config_['frame_id']
+  m.scale.z = line_width
+  if not args.has_key('color'):
+    m.color = _config_['marker_color']
+
+  _config_['marker_id'] += 1
+
+  return m
+
+
+def marker_line(start, end, **args):
+  global _config_
+  m = marker_base(type=marker.Marker.CYLINDER, **args)
+  marker.align(m, start, end, m.scale.z)
+  return m
+
+
+def marker_point(point, **args):
+  global _config_
+  m = marker_base(type=marker.Marker.SPHERE, **args)
+  m.pose.position = Point(point[0], point[1], point[2])
+  m.scale.x = m.scale.z
+  m.scale.y = m.scale.z
+  return m
+
+
+class LocatedVector:
+  def __init__(self, pos, dir):
+    self.pos = pos
+    self.dir = dir
+
+  def __str__(self):
+    p = self.pos
+    d = self.dir
+    return ('LocatedVector(kdl.Vector(%f, %f, %f), kdl.Vector(%f, %f, %f))'
+            % (p.x(), p.y(), p.z(), d.x(), d.y(), d.z()))
+            
+
+  def __add__(self, other):
+    """Add the second vector to the first vector.
+
+    The resulting vector is located at the first vector's position.
+
+    """
+    return LocatedObject(self.pos, self.dir + other.dir)
+
+  def __sub__(self, other):
+    """Subtract the second vector from the first vector.
+
+    The resulting vector is located at the first vector's position.
+
+    """
+    return LocatedVector(self.pos, self.dir - other.dir)
+
+  def __mul__(self, scalar):
+    """Scale the length of the vector.
+
+    The position remains the same.
+
+    """
+    return LocatedVector(self.pos, self.dir * scalar)
+
+  def compute(self):
+    return self
+
+  def show(self):
+    return [marker_line(self.pos, self.pos + self.dir)]
+
+
+
+class Feature:
+  def __init__(self, type, pos, dir, frame_id):
+    self.type = type
+    self.rel_pos = pos
+    self.rel_dir = dir
+    self.pos = pos
+    self.dir = dir
+    self.frame_id = frame_id
+
+  def transform(self, frame):
+    self.pos = frame.p + self.rel_pos
+    self.dir = frame.M*self.rel_dir
+
+  def v(self):
+    return LocatedVector(self.pos, self.dir)
+
+  def compute(self):
+    return self
+
+  def show(self):
+    #TODO: distinguish between line, plane and point!
+    # assuming line for now.
+    if self.type == 0: # LINE
+      return [marker_line(self.pos - self.dir/2, self.pos + self.dir/2,
+                          color=yellow)]
+    elif self.type == 2: #POINT
+      return [marker_point(self.pos, color=yellow, line_width=0.1)]
+
+
+class Len:
+  def __init__(self, vector):
+    self.vector = vector
+
+  def compute(self):
+    vec = self.vector.compute()
+    return vec.dir.Norm()
+
+  def show(self):
+    global _config_
+    vec = self.vector.compute()
+    len_marker = marker_line(vec.pos, vec.pos + vec.dir,
+                             color=red, line_width=0.08)
+    return self.vector.show() + [len_marker]
+
+
+class D:
+  def __init__(self, start, end):
+    self.start = start
+    self.end = end
+
+  def compute(self):
+    return LocatedVector(self.start.pos, self.end.pos - self.start.pos)
+
+  def show(self):
+    return self.compute().show()
+
+
+class Proj_P:
+  def __init__(self, vec, ref):
+    self.vec = vec
+    self.ref = ref
+    self.result = kdl.Vector()
+
+  def compute(self):
+    vec = self.vec.compute()
+    ref = self.ref.compute()
+    self.result_dir = ref * (kdl.dot(ref.dir, vec.dir) / ref.dir.Norm()) - vec
+    return LocatedVector(ref.pos, self.result_dir.dir)
+
+  def show(self):
+    vec = self.vec.compute()
+    ref = self.ref.compute()
+    res = self.compute()
+    along = res.dir + vec.dir
+
+    markers  = vec.show() + res.show()
+    markers += [marker_line(vec.pos, vec.pos + along)]
+
+    return markers
+
+
+class Proj_A:
+  def __init__(self, vec, ref):
+    self.vec = vec
+    self.ref = ref
+
+  def compute(self):
+    vec = self.vec.compute()
+    ref = self.ref.compute()
+    res = ref * (kdl.dot(ref.dir, vec.dir) / ref.dir.Norm())
+    return LocatedVector(vec.pos, res.dir)
+
+  def show(self):
+    vec = self.vec.compute()
+    ref = self.ref.compute()
+    res = self.compute()
+    perp = res.dir - vec.dir
+    dist_marker = marker_line(ref.pos, ref.pos + perp)
+    return vec.show() + res.show() + [dist_marker]
+
+
+
+# a map from constraint name to a function (Feature x Feature -> Value)
+constraint_functions = {
+  'distance':  lambda (f_t, f_w) : Len(Proj_P(D(f_t, f_w), f_w.v())),
+  'height':   lambda (f_t, f_w) : Len(Proj_A(D(f_t, f_w), f_w.v()))}
+
+class ConstraintDisplay:
+  def __init__(self, base_frame_id):
+    self.tool_features = []
+    self.world_features = []
+    self.constraints = {}
+    self.base_frame_id = base_frame_id
+    self.listener = tf.TransformListener()
+
+  def transform(self):
+    for f in self.tool_features + self.tool_features:
+      try:
+        frame = self.listener.lookupTransform(self.base_frame_id,
+                                              f.frame_id, rospy.Time(0))
+      except:
+        continue
+      f.transform(pm.fromTf(frame))
+
+  def show(self):
+    markers = []
+    for f in self.tool_features:
+      markers += f.show()
+    for f in self.world_features:
+      markers += f.show()
+    for name in self.constraints:
+      _config_['ns'] = name
+      markers += self.constraints[name].show()
+    _config_['ns'] = 'features'
+    return markers
+
+
+
+
+def callback(msg):
+  global _config_
+  for c in msg.constraints:
+    f_tool  = msg2feature(c.tool_feature)
+    f_world = msg2feature(c.world_feature)
+    f_tool.transform(kdl.Frame(kdl.Vector(1,0.2,0.3)))
+    if c.function in constraint_functions:
+      constraint_display.tool_features.append(f_tool)
+      constraint_display.world_features.append(f_world)
+      constraint = constraint_functions[c.function]((f_tool, f_world))
+      constraint_display.constraints[c.name] = constraint
+    else:
+      print "constraint function '%s' not found!" % c.function
+
+  for m in constraint_display.show():
+    marker.publish(m)
+
+
+def transform():
+  global listener, base_frame_id, constraint_display
+
+
+rospy.init_node('feature_vis')
+base_frame_id = rospy.get_param('base_frame', 'baker')
+constraint_display = ConstraintDisplay(base_frame_id)
+
+sub = rospy.Subscriber('/constraint_config', ConstraintConfig, callback)
+
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+  constraint_display.transform()
+  _config_['marker_id'] = 0
+  for m in constraint_display.show():
+    marker.publish(m)
+  rate.sleep()
