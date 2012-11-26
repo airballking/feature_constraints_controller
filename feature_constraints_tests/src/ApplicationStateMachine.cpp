04c2979117810d3cf4bf421ba6d8de1fa45f8e4b
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2012-11-26 10:40:26 +0100

    Added state machine to executive to control the entire flipping action.

---
+++ feature_constraints_tests/src/ApplicationStateMachine.cpp
@@ -0,0 +1,161 @@
+#include "feature_constraints_tests/ApplicationStateMachine.h"
+
+#include "feature_constraints_tests/Conversions.h"
+
+#include "ros/ros.h"
+
+// the state machine that michael shouldn't see ;)
+
+// state flags
+bool initial_state_ = true;
+bool approach_state_ = false;
+bool push_under_state_ = false;
+bool lift_state_ = false;
+bool flip_state_ = false;
+bool final_state_ = false;
+
+// convenience functions that fill the command message for the motion phases
+void emptyCommand(constraint_msgs::ConstraintCommand& target)
+{
+  target.weight.clear();
+  target.pos_lo.clear();
+  target.pos_hi.clear();
+}
+
+void fillCommandApproach(constraint_msgs::ConstraintCommand& target)
+{
+  emptyCommand(target);
+
+  // ...pointing command
+  addConstraintCommand(target, 1.0, -0.05, 0.05);
+  // ...distance command
+  addConstraintCommand(target, 1.0, 0.2, 0.3);
+  // ...align command
+  addConstraintCommand(target, 1.0, -0.3, -0.1);
+  // ...height command
+  addConstraintCommand(target, 1.0, 0.0, 0.01);
+  // ...align front command
+  addConstraintCommand(target, 1.0, -0.05, 0.05);
+}
+
+void fillCommandPushUnder(constraint_msgs::ConstraintCommand& target)
+{
+  emptyCommand(target);
+
+  // ...pointing command
+  addConstraintCommand(target, 1.0, -0.05, 0.05);
+  // ...distance command
+  addConstraintCommand(target, 1.0, -0.02, 0.02);
+  // ...align command
+  addConstraintCommand(target, 1.0, -0.05, 0.05);
+  // ...height command
+  addConstraintCommand(target, 1.0, 0.0, 0.01);
+  // ...align front command
+  addConstraintCommand(target, 1.0, -0.05, 0.05);
+}
+
+void fillCommandLift(constraint_msgs::ConstraintCommand& target)
+{
+  emptyCommand(target);
+
+  // ...pointing command
+  addConstraintCommand(target, 1.0, -0.05, 0.05);
+  // ...distance command
+  addConstraintCommand(target, 1.0, 0.03, 0.07);
+  // ...align command
+  addConstraintCommand(target, 1.0, -0.08, 0.08);
+  // ...height command
+  addConstraintCommand(target, 1.0, 0.1, 0.2);
+  // ...align front command
+  addConstraintCommand(target, 1.0, -0.08, 0.08);
+}
+
+void fillCommandFlip(constraint_msgs::ConstraintCommand& target)
+{
+  emptyCommand(target);
+
+  // ...pointing command
+  addConstraintCommand(target, 1.0, -0.05, 0.05);
+  // ...distance command
+  addConstraintCommand(target, 1.0, 0.03, 0.07);
+  // ...align command
+  addConstraintCommand(target, 1.0, -0.08, 0.08);
+  // ...height command
+  addConstraintCommand(target, 1.0, 0.1, 0.2);
+  // ...align front command
+  // SINGULARITY !!
+  addConstraintCommand(target, 1.0, -1.0, -0.95);
+}
+
+void fillCommandStop(constraint_msgs::ConstraintCommand& target)
+{
+  emptyCommand(target);
+
+  // give all constraints a well-behaved command with zero weight, i.e. do not change
+  // NOTE: this might still causes motion because of the joint limit avoidance
+  // ...pointing command
+  addConstraintCommand(target, 0.0, -0.1, 0.1);
+  // ...distance command
+  addConstraintCommand(target, 0.0, -0.1, 0.1);
+  // ...align command
+  addConstraintCommand(target, 0.0, -0.1, 0.1);
+  // ...height command
+  addConstraintCommand(target, 0.0, -0.1, 0.1);
+  // ...align front command
+  addConstraintCommand(target, 0.0, -0.1, 0.1);
+}
+
+// the actual state machine code
+// returns a flag to signal whether it has finished, i.e. TRUE means the state machine is in 'final state'
+bool updateStateMachineAndWriteCommand(constraint_msgs::ConstraintCommand& constraint_command_msg, bool constraints_fulfilled)
+{
+    if(initial_state_ )
+    {
+      ROS_INFO("Switching to approach-state.");
+      initial_state_ = false;
+      approach_state_ = true;
+      fillCommandApproach(constraint_command_msg);
+      return false;
+    }
+
+    if(approach_state_ && constraints_fulfilled)
+    {
+      ROS_INFO("Switching to push-under-state.");
+      approach_state_ = false;
+      push_under_state_ = true;
+      fillCommandPushUnder(constraint_command_msg);
+      return false;
+    }
+
+    if(push_under_state_ && constraints_fulfilled)
+    {
+      ROS_INFO("Switching to lift-state.");
+      push_under_state_ = false;
+      lift_state_ = true;
+      fillCommandLift(constraint_command_msg);
+      return false;
+    }
+
+    if(lift_state_ && constraints_fulfilled)
+    {
+      ROS_INFO("Switching to flip-state.");
+      lift_state_ = false;
+      flip_state_ = true;
+      fillCommandFlip(constraint_command_msg);
+      return false;
+    }
+    if(flip_state_ && constraints_fulfilled)
+    {
+      ROS_INFO("Switching to final-state.");
+      flip_state_ = false;
+      final_state_ = true;
+      fillCommandStop(constraint_command_msg);
+      return true;
+    }
+    
+     if(final_state_)
+      return true;
+ 
+     // some state but no new event 
+     return false;
+}
--- feature_constraints_tests/src/feature_controller_tester.cpp
+++ feature_constraints_tests/src/feature_controller_tester.cpp
@@ -1,13 +1,32 @@
 // some ros stuff
 #include "ros/ros.h"
-
 // the messages
 #include "constraint_msgs/ConstraintConfig.h"
 #include "constraint_msgs/Constraint.h"
 #include "constraint_msgs/ConstraintCommand.h"
-
-// some convenience functions
+#include "constraint_msgs/ConstraintState.h"
+// some convenience functions to build config message
 #include "feature_constraints_tests/Conversions.h"
+// the state machine
+#include "feature_constraints_tests/ApplicationStateMachine.h"
+
+// constraints_fulfilled_ triggers state transitions, i.e. it's an event
+bool constraints_fulfilled_ = false;
+
+// callback function listens to controller state and figures out when all constraints are met
+void controllerStateCallback(const constraint_msgs::ConstraintState::ConstPtr& msg)
+{
+  constraints_fulfilled_ = !constraints_fulfilled_;
+  
+  for(unsigned int i=0; i<msg->weights.size(); i++)
+  {
+    constraints_fulfilled_ &= (msg->weights[i] < 1.0);
+  }  
+
+  if(constraints_fulfilled_){
+    ROS_INFO("All constraints are fulfilled.");
+  }
+}
 
 int main(int argc, char **argv)
 {
@@ -18,6 +37,9 @@ int main(int argc, char **argv)
   // advertise topics
   ros::Publisher constraint_config_publisher = n.advertise<constraint_msgs::ConstraintConfig>("constraint_config", 1, true);
   ros::Publisher constraint_command_publisher = n.advertise<constraint_msgs::ConstraintCommand>("constraint_command", 1, true);
+
+  // subscribe to state topic
+  ros::Subscriber constraint_state_subscriber = n.subscribe("constraint_state", 1, controllerStateCallback);
 
   // construct messages...
   // ... features
@@ -56,29 +78,29 @@ int main(int argc, char **argv)
   constraint_config_msg.constraints.push_back(height_constraint);
   constraint_config_msg.constraints.push_back(align_front_constraint);
 
-  // ...commands...
-  // ...pointing command
-  addConstraintCommand(constraint_command_msg, 1.0, 0.0, 0.0);
-  // ...distance command
-  addConstraintCommand(constraint_command_msg, 1.0, 0.2, 2.0);
-  // ...align command
-  addConstraintCommand(constraint_command_msg, 1.0, -0.3, -0.1);
-  // ...height command
-  addConstraintCommand(constraint_command_msg, 1.0, 0.05, 0.2);
-  // ...align front command
-  addConstraintCommand(constraint_command_msg, 1.0, -0.05, 0.05);
-
-  // publish messages
+  // publish constraint configuration message
   constraint_config_publisher.publish(constraint_config_msg);
 
-  ros::Rate loop_rate(10);
-  while(ros::ok())
+  // start application which sends different command messages
+  ros::Rate loop_rate(1);
+  ROS_INFO("Starting executive in initial state.");
+  
+  // another flag which signals that the state machine has finished
+  bool state_machine_finished = false;
+  while(ros::ok() && !state_machine_finished)
   {
+    
+    // the mean state machine that michael should not see ;)
+    state_machine_finished = updateStateMachineAndWriteCommand(constraint_command_msg, constraints_fulfilled_);
+    
     constraint_command_publisher.publish(constraint_command_msg);
   
     ros::spinOnce();
     loop_rate.sleep();
   }
+
   // bye bye
+  ROS_INFO("Terminating executive.");
+
   return 0;
 }
