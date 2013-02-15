4daafb1f1a5913436e46af46f9e6e9f86780a406
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2013-02-15 05:07:18 -0800

    Final tweaking for video shooting.

--- feature_constraints_tests/src/ApplicationStateMachine.cpp
+++ feature_constraints_tests/src/ApplicationStateMachine.cpp
@@ -132,8 +132,8 @@ void fillCommandLeftLift(constraint_msgs::ConstraintCommand& left_target)
   // ...align command
   addConstraintCommand(left_target, 1.0, -0.08, 0.08, -0.1, 0.1);
   // ...height command
-//  addConstraintCommand(left_target, 1.0, 0.15, 0.3, -0.1, 0.1); // WAS WORKING BUT LOOKED PRETTY HIGH!!
-  addConstraintCommand(left_target, 1.0, 0.1, 0.15, -0.1, 0.1);
+  addConstraintCommand(left_target, 1.0, 0.15, 0.3, -0.1, 0.1); // WAS WORKING BUT LOOKED PRETTY HIGH!!
+//  addConstraintCommand(left_target, 1.0, 0.1, 0.15, -0.1, 0.1);
   // ...align front command
   addConstraintCommand(left_target, 1.0, -0.1, -0.05, -0.1, 0.1);
   // ... facing constraint
@@ -152,8 +152,8 @@ void fillCommandLeftFlip(constraint_msgs::ConstraintCommand& left_target)
   // ...align command
   addConstraintCommand(left_target, 1.0, -0.2, 0.2, -0.2, 0.2);
   // ...height command
-//  addConstraintCommand(left_target, 1.0, 0.1, 0.3, -0.2, 0.2); // WAS WORKING BUT LOOKED PRETTY HIGH!!
-  addConstraintCommand(left_target, 1.0, 0.1, 0.15, -0.2, 0.2);
+  addConstraintCommand(left_target, 1.0, 0.1, 0.3, -0.2, 0.2); // WAS WORKING BUT LOOKED PRETTY HIGH!!
+//  addConstraintCommand(left_target, 1.0, 0.1, 0.15, -0.2, 0.2);
   // ...align front command
   // use facing constraint instead of align front to avoid singularity 
   // note: direction vectors of spatula front and plane are perpencicular but relate
